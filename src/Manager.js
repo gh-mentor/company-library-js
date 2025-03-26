1592e439cc958cf5b94710820bc11ccc5716e361
Andrew C. Scoppa <andrew.scoppa@gmail.com>
2025-03-26 05:35:20 -0400

    Updated

--- src/Manager.js
+++ src/Manager.js
@@ -3,12 +3,7 @@ const Employee = require('./Employee');
 
 
 class Manager extends Employee {
-    /*
-    * TODO:
-    * 1) Define a separate method to set the department of the manager.
-    * This method should throw an error if the department is not a string.
-    * 2) The constructor must check that all 4 arguments are provided and that the department is a string.
-    */
+
     constructor(id, name, salary, department) {
       if (!id || !name || !salary || !department) {
         throw new Error('All arguments must be provided');
